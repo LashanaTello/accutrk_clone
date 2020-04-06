e1ccc880d8b4069670ab725849656732c402cdc8
LashanaTello <lashana.tello@gmail.com>
2020-04-06 19:30:03 -0400

    Added method to add media and to get the list of media in the database. Also added methods to get media that's currently checked out and get all media checkout history

--- mediaDB.py
+++ mediaDB.py
@@ -22,3 +22,33 @@ class MediaDatabase(object):
             print("Couldn't connect to media database")
         except:
             print("Something went wrong")
+
+    # returns list of all media in the database
+    @staticmethod
+    def get_all_media():
+        return MediaDatabase.DATABASE[MEDIA_LIST].find({}, {"_id": 0})
+
+    # returns list of all media currently checked out
+    @staticmethod
+    def get_checkout_list():
+        return MediaDatabase.DATABASE[CURRENT_MEDIA_CHECKOUTS].find({}, {"_id": 0})
+
+    # returns list detailing every time a piece of media was ever checked out
+    @staticmethod
+    def get_checkout_history():
+        return MediaDatabase.DATABASE[MEDIA_CHECKOUT_HISTORY].find({}, {"_id": 0})
+
+    @staticmethod
+    def add_media(new_media):
+        result = MediaDatabase.DATABASE[MEDIA_LIST].update_one({"media_barcode": new_media["media_barcode"],
+                                                                "media_title": new_media["media_title"],
+                                                                "media_type": new_media["media_type"]},
+                                                               {"$set":
+                                                                    {"media_barcode": new_media["media_barcode"],
+                                                                     "media_title": new_media["media_title"],
+                                                                     "media_type": new_media["media_type"]}},
+                                                               upsert=True)
+
+        if result.upserted_id is not None:
+            return True
+        return False
