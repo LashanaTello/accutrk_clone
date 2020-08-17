0cd191e7498fc5e867c826f261941c8c9d8016de
LashanaTello <lashana.tello@gmail.com>
2020-08-17 17:33:23 -0400

    Added more functionality to sign in dialog

--- gui/controllers/sign_in_dialog.py
+++ gui/controllers/sign_in_dialog.py
@@ -15,11 +15,19 @@ class SignInDialog(QtWidgets.QDialog, Ui_SignInDialog):
 
     def init_ui(self):
         self.cancelButton.clicked.connect(self.cancel_button_clicked)
+        self.eventList.itemDoubleClicked.connect(self.event_item_double_clicked)
 
     def cancel_button_clicked(self):
         self.close()
 
+    def event_item_double_clicked(self):
+        self.eventList.setCurrentRow(-1)
+
     def fill_in(self, student):
+        activities = Database.get_all_activities()
+        for activity in activities:
+            self.activityList.addItem(activity["activity"])
+        self.activityList.sortItems(0)
         self.semesterList.addItem(Database.get_semester_name())
         self.semesterList.setCurrentRow(0)
         self.studentIDLabel.setText(student["barcode"])
@@ -27,6 +35,7 @@ class SignInDialog(QtWidgets.QDialog, Ui_SignInDialog):
         for a_class in student["enrolled_list"]:
             one_class = a_class["subject"] + " " + a_class["catalog"] + " - " + a_class["section"]
             self.classList.addItem(one_class)
+        self.classList.sortItems(0)
 
 
 if __name__ == '__main__':
