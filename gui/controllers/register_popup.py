9aa1e3f5bbff28a06f2d2ba22bce75fd0d48d100
LashanaTello <lashana.tello@gmail.com>
2020-08-19 01:51:24 -0400

    Added all classes in db to combobox list for user to choose from when registering student and added a completer that suggests classes as you type in the combobox

--- gui/controllers/register_popup.py
+++ gui/controllers/register_popup.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 from gui.RegisterPopup import Ui_Register
+from gui.helperfunctions.helpers import combine_into_class
+from server import Database
 
 
 class RegisterPopup(QtWidgets.QDialog, Ui_Register):
@@ -22,6 +24,20 @@ class RegisterPopup(QtWidgets.QDialog, Ui_Register):
         validator = QtGui.QRegExpValidator(rx)
         self.lastnameInput.setValidator(validator)
         self.firstnameInput.setValidator(validator)
+
+        self.init_ui()
+
+    def init_ui(self):
+        class_list = Database.get_all_classes()
+        formatted_class_list = []
+        for a_class in class_list:
+            formatted_class_list.append(combine_into_class(a_class["subject"], a_class["catalog"], a_class["section"]))
+        self.classListComboBox.addItems(formatted_class_list)
+
+        completer = QtWidgets.QCompleter(formatted_class_list)
+        self.classListComboBox.setCompleter(completer)
+
+        self.classListComboBox.setCurrentIndex(-1)
 
 
 if __name__ == '__main__':
